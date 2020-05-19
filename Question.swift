c7f6fa13af6ab8926b43cf34c67fbd31f11d29d4
Siavash <svshjalali@gmail.com>
2020-05-19 23:29:20 +0900

    Question struct and answer validation

---
+++ Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Questionnaire
+//
+//  Created by Siavash Jalali on 2020/05/19.
+//  Copyright © 2020 shichan-official. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let question: String
+    let answer: Bool
+}
--- ViewController.swift
+++ ViewController.swift
@@ -7,13 +7,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueBtn: UIButton!
     @IBOutlet weak var falseBtn: UIButton!
     
+    let questions = [
+        Question(question: "Today is Friday", answer: true),
+        Question(question: "Everyday is Friday", answer: true),
+        Question(question: "Tomorrow is Monday", answer: false)
+    ]
+    
+    var questionIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        questionText.text = "Today is Friday"
+        progressBar.progress = 0.0
+        updateUi()
     }
     @IBAction func answerPressed(_ sender: UIButton) {
-        print("jank")
+        var answer = false
+        if(sender.currentTitle == "True") {
+            answer = true
+        }
+        
+        if(answer == questions[questionIndex].answer) {
+            // correct answer
+        } else {
+            // wrong answer
+        }
+        
+        if(questionIndex + 1 < questions.count) {
+            questionIndex += 1
+        } else {
+            questionIndex = 0
+        }
+        progressBar.progress = Float(questionIndex) / Float(questions.count)
+        updateUi()
+    }
+    
+    func updateUi() {
+        questionText.text = questions[questionIndex].question
     }
     
 
